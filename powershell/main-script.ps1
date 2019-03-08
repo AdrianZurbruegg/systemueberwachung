cc119ed5fd5a7ca2796e1c75e18b8919b5664056
Adrian Zurbruegg <adrian.zurbruegg@isceco.admin.ch>
2019-03-08 10:56:30 +0100

    0.5.0 PowerShell Main-Script: Get linux information, added linux sql connection

--- powershell/main-script.ps1
+++ powershell/main-script.ps1
@@ -12,9 +12,17 @@ param(
 
 
 # This bit of code establishes the Database connection.
-# Load windows assembly (Connector/.NET driver)
-[System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+# These 2 if loops decide which assembly to load. (Windows or Linux) This is required to enable the database connection.
+if ($hostSystem -eq 'Windows'){
 
+    # Load windows assembly (Connector/.NET driver)
+    [System.Reflection.Assembly]::LoadWithPartialName("MySql.Data")
+}
+if ($hostSystem -eq 'Linux'){
+
+    # Load linux assembly (.NET & MONO driver)
+    add-type -Assembly /opt/microsoft/powershell/6/MySql.Data.dll
+}
 
 # Build connection string for database.
 [string]$sMySQLUserName = 'admin'
@@ -164,11 +172,79 @@ if ($hostSystem -eq 'Linux'){
     # Gets some basic information of the system. (Function has more than 20 lines, since a certain amount of information is required)
     function getBasicInfo{
 
+        # Note: all network specific informations are only about IPv4.
+
+        # Get IP-Address.
+        $ipAddress = hostname -I | awk '{$1=$1};1'
+        $ipAddress
+
+        # Get subnetmask.
+        $subnetmask = ip -o -f inet addr show | awk '/scope global/ {print $4}'
+        $subnetmask
+
+        # Get gateway.
+        $gateway = route -n | grep 'UG[ \t]' | awk '{print $2}'
+        $gateway
+
+        # Get DNS-Servers.
+        $dnsServer = cat /run/systemd/resolve/resolv.conf | tail -2 | tr -d nameserver | awk '{$1=$1};1'
+        $dnsServer
+
+        # Get disks (Volume & Partition)
+        $volumes = df -hlT | grep -v 'tmpfs\|loop' | sed -e 's/  */,/g' -e 's/$/ /g' | awk 'NR!=1' | awk '{$1=$1};1' | cut -d',' '-f5,6' --complement | tr -d 'G'
+        $volumes
+
+        # Get CPU processors.
+        $processor = less /proc/cpuinfo | grep 'model name' | head -n 1 | cut -d':' -f2 | awk '{$1=$1};1'
+        $processor
+
+        $cores = lscpu | grep 'Socket' | cut -d':' -f2 | awk '{$1=$1};1'
+        $cores
+
+        $logicalCores = nproc
+        $logicalCores
+
+        # Get RAM amount.
+        $ram = awk '/MemTotal/ {print $2}' /proc/meminfo
+        $ram = [Math]::Round(($ram / 1024)/1024)
+        $ram
+
+        # Get operating system & manufacturer info.
+        $operatingSystem =  cat /etc/os-release | grep 'PRETTY_NAME' | cut -d'=' -f2 | sed 's/\"//g'
+        $operatingSystem
+
+        $vendor = cat /sys/devices/virtual/dmi/id/sys_vendor
+        $productName = cat /sys/devices/virtual/dmi/id/product_name
+        $manufacturerInfo = $vendor + " " + $productName
+        $manufacturerInfo
+
+        # Get system uptime.
+        $uptime = Get-UpTime
+        $days = $uptime.Days
+        $hours = $uptime.Hours
+        $minutes = $uptime.Minutes
+        $seconds = $uptime.Seconds
+        $uptime = "Days:" + $days + " Hours:" + $hours + " Minutes:" + $minutes + " Seconds:" + $seconds
+        $uptime
+
     }
 
 
     # Gets perfomance specific information about the system.
     function getPerformanceInfo{
+
+        # Get disk usage in percent.
+        $diskUsage = df -hlT | grep -v 'tmpfs\|loop' | awk 'NR!=1' | sed -e 's/  */,/g' -e 's/$/ /g' | awk '{$1=$1};1' | cut -d',' '-f1,6' | tr -d '%'
+        $diskUsage
+
+        # Get CPU usage in percent.
+        $cpuUsage = (100-(vmstat 1 2 | tail -1 | awk '{print $15}'))
+        $cpuUsage
+
+        # Get RAM usage in percent.
+        $ramUsage = free | grep Mem | awk '{print $3/$2 * 100.0}'
+        $ramUsage = [Math]::Round($ramUsage)
+        $ramUsage
 
     }
 
